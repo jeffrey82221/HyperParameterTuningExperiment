82fc8bd8fac3ca3979f189b8db9492a8acfe3cbb
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-19 12:48:03 +0800

    Add XOR training data generator

--- TrainerFunc.py
+++ TrainerFunc.py
@@ -3,40 +3,43 @@ import gc
 from numba import cuda
 import pickle
 import tensorflow as tf
-from SqueezeNet import squeeze_net
+from XORNet import xor_net
 from sklearn.preprocessing import OneHotEncoder
 
 
-def SqueezeNetFunction(squeeze_scale_exp, small_filter_rate, lr_exp):
-  batch_size = 2048
-  f = open('data.p', 'rb')
-  (X_train, y_train), (X_test, y_test) = pickle.load(f)  # cifar100.load_data()
-  oh = OneHotEncoder(sparse=False)
-  oh.fit(y_train)
-  acc_dict = dict()
-  # repeat = 30
-  # sf_rates = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
-  scale = 10**squeeze_scale_exp  # float(sys.argv[1])
-  small_filter_rate = small_filter_rate  # float(sys.argv[2])
+def TrainerFunction(batch_size, lr_exp, momentum, layer_size, layer_count):
+  # parameter initialize
+  batch_size = batch_size
   lr = 10**lr_exp  # float(sys.argv[3])
-  op = tf.keras.optimizers.Nadam(
-      lr=lr)  # , decay=1e-6, momentum=0.9)
-  model = squeeze_net(small_filter_rate=small_filter_rate, squeeze_scale=scale, verbose=False)
-  loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+  momentum = momentum
+  layer_size = layer_size
+  layer_count = layer_count
+
+  op = tf.keras.optimizers.SGD(
+      lr=lr,
+      momentum=momentum,
+      nesterov=True
+  )  # , decay=1e-6, momentum=0.9)
+  model = xor_net(layer_size, layer_count, verbose=False)
+  loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
   model.compile(loss=loss,
                 optimizer=op,
                 metrics=['acc'])
   stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_acc',
                                                 patience=5,
                                                 verbose=1)
-  history = model.fit(X_train / 255.,
-                      oh.transform(y_train),
-                      epochs=10000,
-                      batch_size=batch_size,
-                      validation_data=(X_test / 255.,
-                                       oh.transform(y_test)),
-                      verbose=2,
-                      callbacks=[stop_early], shuffle=True)
+  validation_size = 1000
+  xor_data_generator = get_xor_generator(batch_size)
+  xor_validation_data = get_xor_valid_data(validation_size)
+  history = model.fit_generator(xor_data_generator,
+                                steps_per_epoch=100,  # this is a virtual parameter
+                                epochs=10000,
+                                batch_size=batch_size,
+                                validation_data=xor_validation_data,
+                                verbose=2,
+                                callbacks=[stop_early],
+                                multiprocessing=True
+                                )
   # make sure the input of each pixel is between 0 and 1
   # get final val acc
   final_val_acc = history.history['val_acc'][-1]
---
+++ XOR_Util.py
@@ -0,0 +1,45 @@
+import random
+import numpy as np
+from tensorflow.python.keras.utils.data_utils import Sequence
+import numpy as np
+'''
+from XOR_Util import get_xor_generator
+gen = get_xor_generator(8)
+next(gen)
+'''
+
+
+class get_xor_generator(Sequence):
+  def __init__(self, batch_size):
+    assert batch_size % 4 == 0
+    # make sure the number of the following instances are the same:
+    # (0,0), (0,1), (1,0), (1,1).
+    self.batch_size = batch_size
+
+  def __len__(self):
+    return 100
+
+  @property
+  def shape(self):
+    return (2,)
+
+  def __getitem__(self, item):
+    return self.__next__()
+
+  def __next__(self):
+    zero_zero_instances = np.zeros((int(self.batch_size / 4), 2))
+    one_one_instances = np.ones((int(self.batch_size / 4), 2))
+    one_zero_instances = np.vstack([
+        zero_zero_instances[:, 0],
+        one_one_instances[:, 0]]).T
+    zero_one_instances = np.vstack([
+        one_one_instances[:, 0],
+        zero_zero_instances[:, 0]]).T
+    instances = np.vstack([
+        zero_zero_instances,
+        one_one_instances,
+        one_zero_instances,
+        zero_one_instances
+    ])
+    target = np.logical_xor(instances[:, 0], instances[:, 1])
+    yield instances, target
