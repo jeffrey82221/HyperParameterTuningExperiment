2be4c1d07438d246bcd1537c60860458d1477032
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-19 12:03:34 +0800

    Allow GPU assignment

--- XORNet.py
+++ XORNet.py
@@ -1,4 +1,14 @@
 import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.experimental.set_visible_devices(gpus[1], 'GPU')
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
 
 
 def xor_net(layer_size, layer_count, verbose=True):
