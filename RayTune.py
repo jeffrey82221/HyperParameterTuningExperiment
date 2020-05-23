b103d78596d3fa5685ff828cf07fb9b967ad939a
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-23 14:11:18 +0800

    Fix GPU growth issue

--- RayTune.py
+++ RayTune.py
@@ -44,36 +44,22 @@ import numpy as np
 
 import ray
 from ray import tune
-from ray.tune.schedulers import AsyncHyperBandScheduler
 from ray.tune import register_trainable
 
 register_trainable("target_function", target_function)
 ray.init(num_cpus=4, num_gpus=1)
+'''
 sched = AsyncHyperBandScheduler(
     time_attr="training_iteration",
     metric="mean_acc",
     mode="max",
     max_t=400,
     grace_period=20)
-pbounds = {
-    'batch_size_continuous': (20, 20),
-    # Possibly as small as possible to reduce model construction time.
-    # Effect of large batch size is the same as large lr because
-    # the training batch is repeative (no variance between batches).
-    'lr_exp': (1.02, 1.02),
-    # As large as possible to allows larger initial gradient
-    'momentum': (0.8, 0.8),
-    'layer_size_continuous': (20, 20),
-    # As large as possible to increase model complexity, since no overfitting is presented.)
-    'layer_count_continuous': (1, 1)
-    # As small as possible because large layer count leads to slower optimization.
-}
-
+'''
 
 tune.run(
     target_function,
     name="xor",
-    scheduler=sched,
     stop={
         "mean_accuracy": 0.99,
         "training_iteration": 5,
--- XORNet.py
+++ XORNet.py
@@ -3,9 +3,11 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
   # Restrict TensorFlow to only use the first GPU
   try:
-    tf.config.experimental.set_visible_devices(gpus[1], 'GPU')
+    tf.config.experimental.set_visible_devices(gpus[3], 'GPU')
     logical_gpus = tf.config.experimental.list_logical_devices('GPU')
     print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
   except RuntimeError as e:
     # Visible devices must be set before GPUs have been initialized
     print(e)
