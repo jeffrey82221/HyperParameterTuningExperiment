479576d77833ad25ac378bcc137edb29416f485a
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-23 14:18:01 +0800

    Fix bug

--- XORNet.py
+++ XORNet.py
@@ -3,11 +3,9 @@ gpus = tf.config.experimental.list_physical_devices('GPU')
 if gpus:
   # Restrict TensorFlow to only use the first GPU
   try:
-    tf.config.experimental.set_visible_devices(gpus[3], 'GPU')
-    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
-    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
+    #tf.config.experimental.set_visible_devices(gpus[3], 'GPU')
+    tf.config.experimental.set_memory_growth(gpus[3], True)
+    print(len(gpus), "Physical GPUs,", len(gpus), "Logical GPU")
   except RuntimeError as e:
     # Visible devices must be set before GPUs have been initialized
     print(e)
