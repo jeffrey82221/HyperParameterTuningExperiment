ee1a4c1514c9cc115df76229c6718ef91da89c88
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-23 16:12:29 +0800

    Fix GPU issue by restricting GPU = 1 for each trail and increase batch size & layer size.

--- RayTune.py
+++ RayTune.py
@@ -47,7 +47,7 @@ from ray import tune
 from ray.tune import register_trainable
 
 register_trainable("target_function", target_function)
-ray.init(num_cpus=48, num_gpus=4)
+ray.init(num_cpus=48, num_gpus=3)
 '''
 sched = AsyncHyperBandScheduler(
     time_attr="training_iteration",
@@ -64,19 +64,21 @@ analysis = tune.run(
         "mean_accuracy": 0.99,
         "training_iteration": 3,
     },
-    num_samples=100,
+    num_samples=30,
     config={
-        "batch_size_continuous": tune.grid_search([4]),  # tune.sample_from(lambda spec: np.random.uniform(4, 4000)),
-        "lr_exp": tune.sample_from(lambda spec: np.random.uniform(1, 2)),
+        "batch_size_continuous": tune.grid_search([16384]),  # tune.sample_from(lambda spec: np.random.uniform(4, 4000)),
+        "lr_exp": tune.sample_from(lambda spec: np.random.uniform(1.1, 2)),
         "momentum": tune.sample_from(
-            lambda spec: np.random.uniform(0.4, 0.5)),
-        "layer_size_continuous": tune.grid_search([4]),
+            lambda spec: np.random.uniform(0.1, 0.9)),
+        "layer_size_continuous": tune.grid_search([32]),
         "layer_count_continuous": tune.grid_search([1]),
     },
     resources_per_trial={
         "cpu": 1,
-        "gpu": 0.1
+        "gpu": 1,
+        "extra_gpu":0
     },
+    queue_trials = True
 )
 print("Best trail is", analysis.get_best_trial(metric="mean_accuracy", mode='max', scope='all'))
 print("Best config is", analysis.get_best_config(metric="mean_accuracy", mode='max', scope='all'))
