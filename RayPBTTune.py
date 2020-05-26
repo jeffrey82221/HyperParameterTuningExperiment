267086e75d34368ed86e1f8933a3a91b09a30b4b
jeffrey82221 <jeffrey82221@gmail.com>
2020-05-26 22:04:40 +0800

    Fix error: too many files opened

--- RayPBTTune.py
+++ RayPBTTune.py
@@ -11,10 +11,10 @@ ray.init(num_cpus=40, num_gpus=0)
 pbt = PopulationBasedTraining(
     time_attr="training_iteration", metric='mean_loss',
     mode='min',
-    perturbation_interval=2,
+    perturbation_interval=4,
     hyperparam_mutations={
-        'lr_exp': lambda _: np.random.uniform(-1., 1.),
-        'momentum': lambda _: np.random.uniform(-0.1, 0.1)
+        'lr_exp': lambda : np.random.uniform(-3., 3.),
+        'momentum': lambda : np.random.uniform(0., 1.0)
     })
 analysis = run(
     InitialAccuracyTrainable,
@@ -32,18 +32,19 @@ analysis = run(
         "cpu": 1,
         "gpu": 0
     },
-    sync_on_checkpoint=False,
+    sync_on_checkpoint=True,
+#sync_to_driver = False, 
     num_samples=30,
     config={
         'epochs': 8,
         'batch_size_continuous': 4,
-        'lr_exp': grid_search([-3, -2, -1, 0, 1, 2, 3]),
-        'momentum': sample_from(lambda spec: np.random.uniform(0.1, 0.9)),
+        'lr_exp': sample_from(lambda spec:np.random.uniform(-2.,2.)),
+        'momentum': sample_from(lambda spec: np.random.uniform(0.1, 0.8)),
         'layer_size_continuous': grid_search([2, 4, 8, 16, 32, 64, 128, 256]),
         'layer_count_continuous': grid_search([1, 2, 3])
     },
     local_dir=".",
-    queue_trials=True
+    queue_trials = True
     # restore="./ray_results_xor"
 )
 print("Best Loss Config", analysis.get_best_config(metric="mean_loss", mode='min', scope='all'))
